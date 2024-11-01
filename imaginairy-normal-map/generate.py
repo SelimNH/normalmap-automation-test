fcdc49b1e943068cb6ad7478e0f2fcdf873d2477
SelimJB <selim.bennani@gmail.com>
2024-11-01 21:30:17 +0700

    Generate imqginairy normals

--- imaginairy-normal-map/generate.py
+++ imaginairy-normal-map/generate.py
@@ -6,7 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from Utils import utils
 
-
 images_path = utils.list_images()
 output_folder = "./output"
 
@@ -14,10 +13,14 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 for image in images_path:
-    print(image)
-    img = Image.open(image)
-    img_name = image.split("/")[-1]
-    normal_img = create_normal_map_pil_img(img)
+    img_name = os.path.basename(image)
     normal_img_path = os.path.join(output_folder, img_name.replace(".png", "-normal.png"))
-    normal_img.save(normal_img_path)
+    print(f"Generating normal map for {img_name} to {normal_img_path}")
 
+    img = Image.open(image)
+    if img.mode != "RGB":
+        img = img.convert("RGB")
+        print(f"Converted {img_name} to RGB")
+
+    normal_img = create_normal_map_pil_img(img)
+    normal_img.save(normal_img_path)
